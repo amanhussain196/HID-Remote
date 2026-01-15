4543ee964e499df2e16335f7b63fbfbdda732add
Syed Aman Hussain <amanrockzz8448@gmail.com>
2026-01-16 00:16:21 +0530

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.hidremote"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.hidremote"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         
